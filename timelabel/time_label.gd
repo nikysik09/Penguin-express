7e889a0fefe4af39fb77206d768f4ad198f753d1
Veronika Konopko <veronikakonopkoo@gmail.com>
2026-04-16 23:02:34 +0300

    added time saving after mini game

--- timelabel/time_label.gd
+++ timelabel/time_label.gd
@@ -3,20 +3,17 @@ extends CanvasLayer
 var time_passed: int = 0
 var is_active: bool = true
 
-
 @onready var timer_label: Label = $TimerLabel
 @onready var stopwatch_timer: Timer = $TimerLabel/StopwatchTimer
 
 func _ready():
-	if stopwatch_timer == null:
-		print("ПОМИЛКА: Вузол StopwatchTimer не знайдено! Перевірте назву в дереві сцени.")
-		return
-	
+	time_passed = Global.saved_time
 	update_timer_display()
 	
-	stopwatch_timer.wait_time = 1.0
-	stopwatch_timer.autostart = true
+	if stopwatch_timer == null:
+		return
 	
+	stopwatch_timer.wait_time = 1.0
 	if not stopwatch_timer.timeout.is_connected(_on_timer_timeout):
 		stopwatch_timer.timeout.connect(_on_timer_timeout)
 	
@@ -25,6 +22,7 @@ func _ready():
 func _on_timer_timeout():
 	if is_active:
 		time_passed += 1
+		Global.saved_time = time_passed
 		update_timer_display()
 
 func update_timer_display():
@@ -32,11 +30,3 @@ func update_timer_display():
 		var minutes = time_passed / 60
 		var seconds = time_passed % 60
 		timer_label.text = "%02d:%02d" % [minutes, seconds]
-
-func stop_timer():
-	is_active = false
-	stopwatch_timer.stop()
-
-func resume_timer():
-	is_active = true
-	stopwatch_timer.start()
